e599083af7af4b537c7346df6b8e5fa060d17e5c
Manav <manavgori104@gmail.com>
2024-07-24 11:55:31 +0530

    first commit

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>Electro</h1>
+            <p>Electro is an electronic gadget store, It have a vast variety of electronics products to choose from.</p>  
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>Ahmedabad </li>
+              <li>Email: help@gmail.com</li>
+              <li>Phone: +91 1234567890</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
